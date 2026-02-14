42bac3df858ac8a64eb53ce8e2ab0d374a131305
Juan Sebastian Buitrago Caballero <jbuitrago225@unab.edu.co>
2026-02-13 23:10:12 -0500

    Vista perfil.blade.php creada

---
+++ resources/views/perfil.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Perfil</title>
+</head>
+<body>
+
+<nav>
+    <a href="/perfil">Perfil</a>
+    <a href="/intereses">Intereses</a>
+    <a href="/habilidades">Habilidades</a>
+    <a href="/metas">Metas</a>
+</nav>
+
+<div class="container">
+    <h1>Informacion Personal</h1>
+
+    <ul>
+        <li><strong>Nombre:</strong> Juan  Sebastian Buitrago</li>
+        <li><strong>Edad:</strong> 19 años</li>
+        <li><strong>Lugar de nacimiento:</strong> El banco Magdalena</li>
+        <li><strong>Donde vivo:</strong> Bucaramanga</li>
+        <li><strong>Gmail:</strong> jbuitrago225@unab.edu.co</li>
+        <li><strong>Numero:</strong> 3224451832</li>
+    </ul>
+</div>
+
+</body>
+</html>
