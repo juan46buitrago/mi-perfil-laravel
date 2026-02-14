0e42f3e9740085598b054e5ddbc4bd3bdac882db
Juan Sebastian Buitrago Caballero <jbuitrago225@unab.edu.co>
2026-02-13 23:51:06 -0500

    Vista metas.blade.php creada

---
+++ resources/views/metas.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Metas</title>
+</head>
+<body>
+
+<nav>
+    <a href="/perfil">Perfil</a>
+    <a href="/perfil/intereses">Intereses</a>
+    <a href="/perfil/habilidades">Habilidades</a>
+    <a href="/perfil/metas">Metas</a>
+</nav>
+
+<div class="container">
+    <h1>Objetivos Profesionales</h1>
+
+    <ul>
+        <li>Tener un trabajo estable</li>
+        <li>Conseguir las habilidades suficientes para desenvolverme en un espacio laboral</li>
+        <li>desenvolverme espacios que ayuden en mi desarrollo profesional</li>
+    </ul>
+</div>
+
+</body>
+</html>
